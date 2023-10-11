bbc4384667afa355c9b337c1161cc69e3015fa6f
buggyGrandma <alireza.kh3221@gmail.com>
2023-10-11 19:05:50 +0330

    Displaying Critic Score

---
+++ src/components/GameCard/CriticScore/index.tsx
@@ -0,0 +1,15 @@
+import { Badge } from "@chakra-ui/react";
+
+interface props {
+  score: number;
+}
+const CriticScore = ({ score }: props) => {
+  let color = score > 75 ? "green" : score > 60 ? "yellow" : " ";
+  return (
+    <Badge colorScheme={color} fontSize="14px" paddingX={2} borderRadius="4px">
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
